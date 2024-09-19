eb5ee37256fe35baf5f65a714849c53f4c0511a0
GustavoMendozaAren <stavmdza@gmail.com>
2024-09-18 22:48:29 -0600

    MainMenuConMejoresAnimaciones

--- BrackeysGameJamProject/Assets/2.Scripts/1.MainMenuScripts/MainMenuManager.cs
+++ BrackeysGameJamProject/Assets/2.Scripts/1.MainMenuScripts/MainMenuManager.cs
@@ -5,7 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [Header ("PANELES")]
     [SerializeField] private GameObject mainMenuPanel, optionsPanel, creditsPanel;
+
+    [Header ("OBJETOS")]
+    [SerializeField] private GameObject lunaImage;
+    [SerializeField] private GameObject solImage;
+
+    [Header("ANIMATORS")]
+    [SerializeField] private Animator lunaAnim;
+    [SerializeField] private Animator solAnim;
+
+
+
+    private void Start()
+    {
+        lunaAnim = lunaImage.GetComponent<Animator>();
+        solAnim = solImage.GetComponent<Animator>();
+    }
     public void PlayBttn()
     {
         SceneManager.LoadScene("TestScene1");
@@ -13,14 +30,38 @@ public class MainMenuManager : MonoBehaviour
 
     public void OptionsBttn()
     {
-        optionsPanel.SetActive(!optionsPanel.activeSelf);
-        mainMenuPanel.SetActive(!mainMenuPanel.activeSelf);
+        optionsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+        lunaAnim.SetBool("InOptions", true);
+
+        solAnim.SetBool("OutMainSol", true);
+    }
+
+    public void OptionsBttnExit()
+    {
+        optionsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+        lunaAnim.SetBool("InOptions", false);
+
+        solAnim.SetBool("OutMainSol", false);
     }
 
     public void CreditsBttn()
     {
-        creditsPanel.SetActive(!creditsPanel.activeSelf);
-        mainMenuPanel.SetActive(!mainMenuPanel.activeSelf);
+        creditsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+        solAnim.SetBool("InCredits", true);
+
+        lunaAnim.SetBool("OutMainLuna", true);
+    }
+
+    public void CreditsBttnExit()
+    {
+        creditsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+        solAnim.SetBool("InCredits", false);
+
+        lunaAnim.SetBool("OutMainLuna", false);
     }
 
     public void ExitBttn()
