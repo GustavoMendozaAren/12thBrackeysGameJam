934190f7f4779ec7a6fd56719914094236ceba7d
GustavoMendozaAren <stavmdza@gmail.com>
2024-10-26 01:35:24 -0600

    LastBuild

--- BrackeysGameJamProject/Assets/2.Scripts/5.ExtraManagers/StaticVariables.cs
+++ BrackeysGameJamProject/Assets/2.Scripts/5.ExtraManagers/StaticVariables.cs
@@ -1,11 +1,15 @@
 public static class StaticVariables
 {
     public static int diasTranscurridos = 1;
+
     public static int cantidadAldeanosTotales = 0;
     public static int cantidadAldeanosDisponibles = 0;
     public static int cantidadBuildersTotales = 0;
     public static int cantidadBuildersDisponibles = 0;
 
     public static int enemiesSpeed = 1;
+
+    public static float proyectileSpeed = 10f;
+    public static float rateOfFire = 1f;
 
 }
