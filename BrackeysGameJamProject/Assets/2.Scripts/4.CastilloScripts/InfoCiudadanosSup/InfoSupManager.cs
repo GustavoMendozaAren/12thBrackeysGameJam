7dd22b697519035a48359fcb0a0103a49cd7b039
GustavoMendozaAren <stavmdza@gmail.com>
2025-01-14 17:13:31 -0600

    NuevaFormaDeVisualizarBuildersYGuerreros

--- BrackeysGameJamProject/Assets/2.Scripts/4.CastilloScripts/InfoCiudadanosSup/InfoSupManager.cs
+++ BrackeysGameJamProject/Assets/2.Scripts/4.CastilloScripts/InfoCiudadanosSup/InfoSupManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class InfoSupManager : MonoBehaviour
@@ -7,9 +8,11 @@ public class InfoSupManager : MonoBehaviour
     [Header("WarriorStuff")]
     [SerializeField] private GameObject iconoCeatedW;
     [SerializeField] private GameObject iconoInactiveW;
-    [SerializeField] private GameObject bttnW;
+    [SerializeField] private GameObject bttnCloseW;
     [SerializeField] private GameObject infoWSup;
     [SerializeField] private Animator infoWSupAnim;
+    [SerializeField] private TextMeshProUGUI warriorCreatedTxt;
+    [SerializeField] private TextMeshProUGUI warriorInactiveTxt;
 
     [Header("BuilderStuff")]
     [SerializeField] private GameObject iconoCeatedB;
@@ -17,19 +20,25 @@ public class InfoSupManager : MonoBehaviour
     [SerializeField] private GameObject bttnCloseB;
     [SerializeField] private GameObject infoBSup;
     [SerializeField] private Animator infoBSupAnim;
-
-    private bool builderBttnActive = false;
+    [SerializeField] private TextMeshProUGUI builderCreatedTxt;
+    [SerializeField] private TextMeshProUGUI builderInactiveTxt;
 
     private void Start()
     {
         infoBSupAnim = infoBSup.GetComponent<Animator>();
+        infoWSupAnim = infoWSup.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        ActualizarTextoBuilder();
+        ActualizarTextoWarrior();
     }
 
     public void OpenBuilderSupBttn()
     {
         bttnCloseB.SetActive(true);
         infoBSupAnim.SetBool("BuilderOpen", true);
-        builderBttnActive = true;
 
         StartCoroutine(BuilderIconosExtra());
     }
@@ -38,7 +47,6 @@ public class InfoSupManager : MonoBehaviour
     {
         bttnCloseB.SetActive(false);
         infoBSupAnim.SetBool("BuilderOpen", false);
-        builderBttnActive = false;
 
         iconoCeatedB.SetActive(false);
         iconoInactiveB.SetActive(false);
@@ -46,12 +54,19 @@ public class InfoSupManager : MonoBehaviour
 
     public void OpenWarriorSupBttn()
     {
+        bttnCloseW.SetActive(true);
+        infoWSupAnim.SetBool("InfoOpenW", true);
 
+        StartCoroutine(WarriorIconosExtra());
     }
 
     public void CloseWarriorSupBttn()
     {
+        bttnCloseW.SetActive(false);
+        infoWSupAnim.SetBool("InfoOpenW", false);
 
+        iconoCeatedW.SetActive(false);
+        iconoInactiveW.SetActive(false);
     }
 
     IEnumerator BuilderIconosExtra()
@@ -59,5 +74,24 @@ public class InfoSupManager : MonoBehaviour
         yield return new WaitForSeconds(.3f);
         iconoCeatedB.SetActive(true);
         iconoInactiveB.SetActive(true);
+    }
+
+    IEnumerator WarriorIconosExtra()
+    {
+        yield return new WaitForSeconds(.3f);
+        iconoCeatedW.SetActive(true);
+        iconoInactiveW.SetActive(true);
+    }
+
+    private void ActualizarTextoBuilder()
+    {
+        builderCreatedTxt.text = StaticVariables.cantidadBuildersTotales + "/5";
+        builderInactiveTxt.text = "" + StaticVariables.cantidadBuildersDisponibles;
+    }
+
+    private void ActualizarTextoWarrior()
+    {
+        warriorCreatedTxt.text = StaticVariables.cantidadAldeanosTotales + "/10";
+        warriorInactiveTxt.text = "" + StaticVariables.cantidadAldeanosDisponibles;
     }
 }
