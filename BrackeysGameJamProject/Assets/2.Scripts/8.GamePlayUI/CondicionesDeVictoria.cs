ce8527bf5086ea934eee5d60b5b8fad8bcaa5740
GustavoMendozaAren <stavmdza@gmail.com>
2025-01-27 17:43:27 -0600

    CambiosParaMerge

--- BrackeysGameJamProject/Assets/2.Scripts/8.GamePlayUI/CondicionesDeVictoria.cs
+++ BrackeysGameJamProject/Assets/2.Scripts/8.GamePlayUI/CondicionesDeVictoria.cs
@@ -7,28 +7,22 @@ public class CondicionesDeVictoria : MonoBehaviour
     [Header("PANELES")]
     [SerializeField] private GameObject VictoryPanel;
     [SerializeField] private GameObject DefeatedPanel;
-    private MusicManager musicManager;
 
     private void Start()
     {
         VictoryPanel.SetActive(false);
         DefeatedPanel.SetActive(false);
-        GameObject instanciaMusic = GameObject.Find("Music");
-        musicManager = instanciaMusic.GetComponent<MusicManager>();
-        musicManager.OnGameStateChanged(0);
     }
 
     public void Victoria()
     {
         VictoryPanel.SetActive(true);
         Time.timeScale = 0f;
-        musicManager.OnGameStateChanged(1);
     }
 
     public void Derrota()
     {
         DefeatedPanel.SetActive(true);
         Time.timeScale = 0f;
-        musicManager.OnGameStateChanged(2);
     }
 }
