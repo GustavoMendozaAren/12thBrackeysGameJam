e3507a013d260f3be64c00404b352013d8c3ce64
GustavoMendozaAren <stavmdza@gmail.com>
2025-03-27 21:51:42 -0600

    MejorandoSpawnerManager

--- BrackeysGameJamProject/Assets/2.Scripts/6.EnemigosScripts/Spawner/SpawnerManagerScript.cs
+++ BrackeysGameJamProject/Assets/2.Scripts/6.EnemigosScripts/Spawner/SpawnerManagerScript.cs
@@ -1,25 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnerManagerScript : MonoBehaviour
 {
+    [SerializeField] private int[] basicoEnCadaHorda;
+    [SerializeField] private int[] healerEnCadaHorda;
+    [SerializeField] private int[] tankEnCadaHorda;
+
     [Header("SPAWNERS")]
     [SerializeField] private GameObject[] spawners; // 0-Basic, 1-Healer, 2-Tank
     MusicManager musicManager;
+
+    private int cantidadDeHordas;
     private void Awake()
     {
         GameObject instanciaMusic = GameObject.Find("Music");
         musicManager = instanciaMusic.GetComponent<MusicManager>();
         musicManager.DayMusic(true);
     }
+
+    private void Start()
+    {
+        cantidadDeHordas = ObtenerMayorLongitud(basicoEnCadaHorda, healerEnCadaHorda, tankEnCadaHorda);
+    }
     public void ActivateSpawner()
     {
-        if(StaticVariables.diasTranscurridos == 1)
-        {
-            Round1(true);
-            musicManager.DayMusic(false);
-        }
+        ChecarSiHayDistintosEnemigos();
+        
+        //if (StaticVariables.diasTranscurridos == 1)
+        //{
+        //    Round1(true);
+        //   musicManager.DayMusic(false);
+        //}
         //foreach (GameObject spawner in spawners)
         //{
         //    spawner.gameObject.SetActive(true);
@@ -28,11 +44,16 @@ public class SpawnerManagerScript : MonoBehaviour
 
     public void DeactivateSpawners()
     {
-        if (StaticVariables.diasTranscurridos == 1)
+        for (int i = 0; i < spawners.Length; i++)
         {
-            Round1(false);
-            musicManager.DayMusic(true);
+            spawners[i].SetActive(false);
         }
+
+        //if (StaticVariables.diasTranscurridos == 1)
+        //{
+        //    Round1(false);
+        //    musicManager.DayMusic(true);
+        //}
         //foreach (GameObject spawner in spawners)
         //{
         //    spawner.gameObject.SetActive(false);
@@ -44,5 +65,29 @@ public class SpawnerManagerScript : MonoBehaviour
     private void Round1(bool active)
     {
         spawners[0].SetActive(active);
+    }
+
+    private int ObtenerMayorLongitud(int[] array1, int[] array2, int[] array3)
+    {
+        // Comparar las longitudes de los tres arreglos
+        return Math.Max(array1.Length, Math.Max(array2.Length, array3.Length));
+    }
+
+    private void ChecarSiHayDistintosEnemigos()
+    {
+        if (basicoEnCadaHorda.Length == 0)
+            spawners[0].SetActive(false);
+        else
+            spawners[0].SetActive(true);
+
+        if (healerEnCadaHorda.Length == 0)
+            spawners[1].SetActive(false);
+        else
+            spawners[1].SetActive(true);
+
+        if (tankEnCadaHorda.Length == 0)
+            spawners[2].SetActive(false);
+        else
+            spawners[2].SetActive(true);
     }
 }
