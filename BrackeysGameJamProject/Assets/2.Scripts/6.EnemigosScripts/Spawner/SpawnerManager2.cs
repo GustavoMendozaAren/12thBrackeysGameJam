25d9f29f53ce4184a6a5cc4c1bdf41cf3778d2a6
GustavoMendozaAren <stavmdza@gmail.com>
2025-04-15 18:38:26 -0600

    SpawnersFuncionales

--- BrackeysGameJamProject/Assets/2.Scripts/6.EnemigosScripts/Spawner/SpawnerManager2.cs
+++ BrackeysGameJamProject/Assets/2.Scripts/6.EnemigosScripts/Spawner/SpawnerManager2.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,28 +9,35 @@ public class SpawnerManager2 : MonoBehaviour
     [SerializeField] private GameObject enemyPrefab1;  // Prefab del enemigo
     [SerializeField] private GameObject enemyPrefab2;  // Prefab del enemigo
     [SerializeField] private GameObject enemyPrefab3;  // Prefab del enemigo
-    [SerializeField] private Transform[] waypoints;   // Puntos que seguirán los enemigos
-    //[SerializeField] private int enemyCount = 5;      // Cantidad de enemigos a instanciar
+    [SerializeField] private Transform[] waypointsS1;   // Puntos que seguirán los enemigos
+    [SerializeField] private Transform[] waypointsS2;   // Puntos que seguirán los enemigos
+    [SerializeField] private Transform[] waypointsS3;   // Puntos que seguirán los enemigos
+
+    [Header("BasicEnemies")]
+    [SerializeField] private int[] enemigosBasicosPorHorda;
+    private int enemigosBasicosIndex;
+    private int enemyBasicCounter = 0;
+
+    [Header("HealerEnemies")]
+    [SerializeField] private int[] enemigosHealerPorHorda;
+    private int enemigosHealerIndex;
+    private int enemyHealerCounter = 0;
 
     [Header("Spawn Settings")]
     [SerializeField] private Transform[] spawnPoint;    // Punto de spawn de los enemigos
-    [SerializeField] private int spawnDelay = 5; // Retraso entre cada spawn
 
     // Time Stuff
     private float timeRemaining = 180f;
     private bool timerIsRunning = false;
-    private int smallCounter = 0;
-    private int minutes;
 
     // New Time stuff
-    private bool spawnNow = false;
     private float temporizador;
     [SerializeField] private float tiempoEntreSpawns = 2f;
 
-    private int enemyCounter = 0;
-
     private void Start()
     {
+        enemigosBasicosIndex = -1;
+        enemigosHealerIndex = -1;
         temporizador = tiempoEntreSpawns;
     }
 
@@ -42,21 +50,11 @@ public class SpawnerManager2 : MonoBehaviour
         }
     }
 
-
     void StopWatchFunc()
     {
-        minutes = (int)timeRemaining;
         if (timeRemaining > 0)
         {
             timeRemaining -= Time.deltaTime * StaticVariables.enemiesSpeed;
-            if (EsDivisibleEntreN(minutes, spawnDelay))
-            {
-                smallCounter++;
-            }
-            else
-            {
-                smallCounter = 0;
-            }
         }
         else
         {
@@ -71,119 +69,65 @@ public class SpawnerManager2 : MonoBehaviour
 
         if (temporizador <= 0f)
         {
-            spawnNow = true;
             temporizador = tiempoEntreSpawns;
-        }
-        else
-        {
-            spawnNow = false;
+            EnemigoBasicoSpawn();
         }
     }
 
     // Esto va en el boton de terminar noche
     public void SpawnEnemies()
     {
+        enemigosBasicosIndex++;
         timeRemaining = 180f;
         timerIsRunning = true;
-        enemyCounter = 0;
-        ActiveSpawnersMethod();
+        enemyBasicCounter = 0;
     }
 
-    void ActiveSpawnersMethod()
+    void EnemigoBasicoSpawn()
     {
-        if (StaticVariables.diasTranscurridos < 2)
+        if (enemyBasicCounter < enemigosBasicosPorHorda[enemigosBasicosIndex])
         {
-            // 7 enemigos basicos
-            //FirstSpawnerActive();
-            PruebaNumero1(7);
-        }
-        else if (StaticVariables.diasTranscurridos < 3)
-        {
-            // 7 basicos por ambos lados y 1 helaer
-            FirstSpawnerActive();
-            SecondSpawnerActive();
-        }else if (StaticVariables.diasTranscurridos < 4)
-        {
-            // 7 basicos por todos lados, 2 healer y 1 tanque
-            FirstSpawnerActive();
-            SecondSpawnerActive();
-        }
-
-    }
-
-    void PruebaNumero1(int cantidad)
-    {
-        if (spawnNow) 
-        {
-            int _cantidad = 0;
-
-            if (_cantidad < cantidad)
+            if (StaticVariables.diasTranscurridos < 2) 
             {
-                GameObject enemy = Instantiate(enemyPrefab1, spawnPoint[0].position, Quaternion.identity);
-                enemy.GetComponent<EnemyMovement>().SetWaypoints(waypoints);
-                enemyCounter++;
+                BasicEnemigo(0, waypointsS1, Quaternion.identity);
+            }
+            else if (StaticVariables.diasTranscurridos < 3)
+            {
+                BasicEnemigo(0, waypointsS1, Quaternion.identity);
+                BasicEnemigo(1, waypointsS2, Quaternion.AngleAxis(180f, Vector3.up));
             }
         }
     }
 
-    void FirstSpawnerActive()
+    void EnemigoHealerSpawn()
     {
-        if (spawnNow)
+        if (enemyHealerCounter < enemigosHealerPorHorda[enemigosHealerIndex])
         {
-            AppearBasicEnemies(7);
-        }
-    }
-
-    void SecondSpawnerActive()
-    {
-        if (smallCounter == 1)
-        {
-            if (enemyCounter < (StaticVariables.diasTranscurridos * 2))
+            if (StaticVariables.diasTranscurridos < 2)
             {
-                //AppearBasicEnemies();
+                HealerEnemigo(0, waypointsS1, Quaternion.identity);
             }
         }
     }
 
-    void ThirdSpawnerActive()
+    void BasicEnemigo(int index, Transform[] waypoints, Quaternion angle)
     {
-        if (smallCounter == 1)
-        {
-            if (enemyCounter < (StaticVariables.diasTranscurridos * 2))
-            {
-                //AppearBasicEnemies();
-            }
-        }
-    }
-
-    void AppearBasicEnemies(int cantidad)
-    {
-        int _cantidad = 0;
-
-        if (_cantidad < cantidad)
-        {
-            GameObject enemy = Instantiate(enemyPrefab1, spawnPoint[0].position, Quaternion.identity);
-            enemy.GetComponent<EnemyMovement>().SetWaypoints(waypoints);
-            enemyCounter++;
-        }
-    }
-
-    void AppearMedicEnemies()
-    {
-        GameObject enemy = Instantiate(enemyPrefab2, spawnPoint[0].position, Quaternion.identity);
+        GameObject enemy = Instantiate(enemyPrefab1, spawnPoint[index].position, angle);
         enemy.GetComponent<EnemyMovement>().SetWaypoints(waypoints);
-        enemyCounter++;
+        enemyBasicCounter++;
     }
 
-    void AppearTankEnemies()
+    void HealerEnemigo(int index, Transform[] waypoints, Quaternion angle)
     {
-        GameObject enemy = Instantiate(enemyPrefab3, spawnPoint[0].position, Quaternion.identity);
+        GameObject enemy = Instantiate(enemyPrefab2, spawnPoint[index].position, angle);
         enemy.GetComponent<EnemyMovement>().SetWaypoints(waypoints);
-        enemyCounter++;
+        enemyBasicCounter++;
     }
 
-    bool EsDivisibleEntreN(int num, int divisor)
+    void TankEnemigo(int index, Transform[] waypoints, Quaternion angle)
     {
-        return num % divisor == 0;
+        GameObject enemy = Instantiate(enemyPrefab3, spawnPoint[index].position, angle);
+        enemy.GetComponent<EnemyMovement>().SetWaypoints(waypoints);
+        enemyBasicCounter++;
     }
 }
